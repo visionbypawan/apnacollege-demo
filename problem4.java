3ab380ab79747daf5561d5ae0c9434f542b44613
pawan <glbitm3@gmail.com>
2026-05-28 22:21:15 +0530

    Update problem4.java

--- problem4.java
+++ problem4.java
@@ -6,7 +6,7 @@ public class problem4{
         int num=sc.nextInt();
         int count=0;
         while(num>0){
-            num=num%10;
+            num=num/10;
             count++;
         }
         System.out.print("the total digit is "+count);
